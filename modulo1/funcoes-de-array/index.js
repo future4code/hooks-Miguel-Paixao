1c33fe9a9d1603824c9bf15fd0e26f7bcc820685
Miguel-LPX <miguelpaixao20@gmail.com>
2022-03-10 23:43:44 -0300

    asicionei leitura de codigo

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,19 @@
+// --- Leitura de codigo
+
+///   a) Vai ser impresso a array completa por 3 vzes
+
+//// 2. a) Retorna esse item  'Amanda Rangel', 'Laís Petra', 'Letícia Chijo' 
+
+///// 3. a) Retorna { nome: 'Amanda Rangel', apelido: 'Mandi' },
+   //      { nome: 'Laís Petra', apelido: 'Laura' }
+
+``` Não consegui me concentrar em fazer o exercio a mente totalmente focada em outra coisa prinicipalemte na saude
+
+Ainda inventei de fazer uma vaquinha virtual pra arrecagar um dinheiro pra paagar uns remedios que eu precisava
+
+meu foco total tava em fazer uns exames medicos e segundo a psiquiatra se der tudo certo e eu tratar a Tireoide posso voltar a ter uma vida normal mais ativo com melhor concentração e etc... 
+
+então eu tenho consciencia o quanto o curso é importante e não pretendo desistir assim como todo dia tou na luta pra cuidar da minha saude
+
+amanhã/hoje mesmo ja acordo cedo pra fazer o exame de tireoide e boa sorte pra mim. 
+```
