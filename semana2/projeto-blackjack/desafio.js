b43dfb13107ddc9cc1659c0cb50136ad21bfdeab
Miguel-LPX <miguelpaixao20@gmail.com>
2022-03-11 23:45:03 -0300

    ProjetoAfazer

---
+++ semana2/projeto-blackjack/desafio.js
@@ -0,0 +1,24 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+```Não consegui focar essa semana pra fazer o projeto, eu aproveitei o carnal pra me organizar em outros setores e tbm de correr atras de marcar exames medico e não consegui 
+ter foco nesse projeto
+pois me sentia ja esgotado fisicamente das coisas que ja vinha fazendo
+Mas tou botando fé que a partir do mes que vem tudo vai melhorar em relação a saude organização e foco
+e essa semana vou tentar rever de pouquinho puquinho algumas coisas mais pesada com laço de repetição funçoes e call back
+Sei que essa área nao adiante correr contra o tempo e nem adianta se atropelar
+por que o desenvolvimento e aprendizado só vem com a repetição e com o tempo
+
+
+
+```
