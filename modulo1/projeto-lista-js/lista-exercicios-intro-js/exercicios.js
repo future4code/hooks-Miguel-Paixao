a9506234fe8463f68bc89c67ba2e98b294f62fbc
Miguel-LPX <miguelpaixao20@gmail.com>
2022-04-13 21:06:52 -0300

    lista java script

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -17,65 +17,118 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+//function calculaAreaRetangulo(altura, largura) {
 
-}
+    //    altura = Number(prompt("Digite a altura"))
+    //    largura = Number(prompt("Digite a largura"))
+
+    //   let areaRetangulo = altura * largura
+    //   let resultado = areaRetangulo
+
+    //  console.log(resultado) 
+
+// }
+// calculaAreaRetangulo()
 
 // EXERCÍCIO 02
-function imprimeIdade() {
-  // implemente sua lógica aqui
+//function imprimeIdade(ano, nascimento ) {
 
-}
+//  ano = Number(prompt("Digite o ano"))
+// nascimento = Number(prompt("Digite o nascimento"))
+
+//   let idadeAtual = ano - nascimento
+//   let resultado = idadeAtual
+
+//   console.log(resultado)
+//   }
+
+//  imprimeIdade()
+
+
 
 // EXERCÍCIO 03
-function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+// function calculaIMC(peso, altura) {
+ 
+// let pesoAtual =  peso 
+//  let alturaAtual = altura 
+//   let imc = pesoAtual / (alturaAtual * alturaAtual)
+//   return imc
 
-}
+// }
+
+
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+function imprimeInformacoesUsuario(nome, idade, email) {
+ //   const nomeUsuario = nome
+//   const idadeUsuario = idade
+//   const emailUsuario = email
 
-}
+//   console.log(` Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e meu email é ${emailUsuario}.  `)
+
+ }
+//   imprimeInformacoesUsuario("Miguel", 31, "miguelpaixao20@gmail.com" )
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+  let corFavorita = []
+
+  const cor1 = prompt("Insira uma cor")
+  const cor2 = prompt("Insira outra cor")
+  const cor3 = prompt("Insira mais uma cor")
+
+ corFavorita.push(cor1)
+ corFavorita.push(cor2)
+ corFavorita.push(cor3)
+console.log(corFavorita)
 
 }
+
+
 
 // EXERCÍCIO 06
-function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
+// function retornaStringEmMaiuscula(string) {
+// let palavra = string
+// let maiuscula = palavra.toUpperCase()
+//  return maiuscula
 
-}
+// }
+
+//console.log(retornaStringEmMaiuscula('banana'))
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+
+  
 
 }
 
 // EXERCÍCIO 08
-function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+// function checaStringsMesmoTamanho(string1, string2) {
+//  string1 = "abc" 
+//  lstring2 = "cba"
 
-}
+// let tamanhoString = string1.includes & string2.includes()
+// return tamanhoString
+// }
+
+// checaStringsMesmoTamanho()
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+  
+  return array[0]
 
 }
+console.log(retornaPrimeiroElemento([1,2,3]))
 
-// EXERCÍCIO 10
-function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
+// // EXERCÍCIO 10
+// function retornaUltimoElemento(array) {
+//   array = ["macã", "banana", "goiaba" ]
+//   return array[2]
 
-}
+// }
+// console.log(retornaUltimoElemento([]))
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
@@ -95,9 +148,26 @@ function checaRenovacaoRG() {
 
 }
 
+
+//Nesta temos a variável day_array com os dias da semana, a variável date_full que recebe o input da data e temos uma função chamada calcularBissexto que é onde faz a verificação da data.
+
+  //A verificação feita na função calcularBissexto é:
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+ 
+  let day_array = 7
+  let date_full = 24
+if (ano % 4 == 0 & ano % 100  ==! 0 ){
+
+  console.log("ano bissexto")
+}
+
+checaAnoBissexto()
+
+
+//       Se ano % 4 é == 0 E ano módulo 100 é diferente de 0 então, bissexto=true
+//       Se ano módulo 400 é 0 então, bissexto=true;
+//       Qualquer caso diferente então, bissexto=false;
 
 }
 
---
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/tentativas.js
@@ -0,0 +1,100 @@
+
+
+// 1-----
+// function calculaAreaRetangulo() {
+//     let areaRetangulo = 0
+//     let lado = 5
+//     let altura = 3
+//     areaRetangulo = lado * altura
+//     return areaRetangulo
+
+//1------------
+
+//function calculaAreaRetangulo() {
+  //     let altura = Number(prompt("Digite altura"))
+  //     let largura = Number(prompt("Digite largura"))
+  //     let areaRetangulo = altura * largura
+  //     return areaRetangulo
+
+// 2 -------
+
+
+// let area = altura * largura
+//  return area
+
+// let esperado = altura * largura
+// return espera
+
+// let alturaEntrada = 3
+// let larguraEntrada = 5
+// let resultado = larguraEntrada * larguraEntrada
+// return resultado
+
+// 3 ---------------------------------
+
+
+// altura = 1.78 * 2
+//   peso = 62 
+//   let imc = peso / altura
+//   return imc 
+
+// peso = 62
+//   altura = 1.78
+//   let imc = peso / (altura * altura)
+//   return imc
+
+
+// function imprimeInformacoesUsuario(nome, idade, email) {
+//   const nomeUsuario = nome
+//   const idadeUsuario = idade
+//   const emailUsuario = email
+
+//   console.log(` Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e meu email é ${emailUsuario}.  `)
+
+// }
+//   imprimeInformacoesUsuario("Miguel", 31, "miguelpaixao20@gmail.com" )
+
+//_____--------------------------------------------------------------
+  
+
+
+
+// function imprimeIdade(anoAtual, anoNascimento) {
+  
+//     const idadeAtual = anoAtual - anoNascimento
+// return idadeAtual 
+
+//   }
+
+//   console.log(imprimeIdade(2022, 1990))
+
+// function retornaPrimeiroElemento(array) {
+  
+//   return array[0]
+
+// }
+// console.log(retornaPrimeiroElemento([1,2,3]))
+
+// -----------------------------------------
+
+// function recebaString () {
+// let str = "alvo"
+// console.log(str.substr(0,1) )
+// console.log(str.substr(3, ))
+// }
+
+// recebaString()
+
+
+function calculaAreaRetangulo(altura, largura) {
+
+  let altura = Number(prompt("Digite a altura"))
+  let largura = Number(prompt("Digite a largura"))
+
+  let areaRetangulo = altura * largura
+  let resultado = areaRetangulo
+
+ return resultado
+
+}
+console.log(calculaAreaRetangulo( ))
